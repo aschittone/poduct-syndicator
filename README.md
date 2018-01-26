173fa2326637b5651da9a29b7b5d4bac4a25602f
github username <a_schittone@yahoo.com>
2018-01-26 03:13:00 -0500

    type readme

--- README.md
+++ README.md
@@ -1,24 +1,16 @@
-# README
+# Event/Product Syndicator
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+This is an API that has two endpoints, which allow an admin user to log in via the ADMIN PANEL at the link below, and create new products/events that the API later syndicates to external event sites. 
 
-Things you may want to cover:
+This API uses a rake task, in addition to the ruby gem, Whenever, in order to schedule syndication every hour, for new products entered into the database. Please see config/schedule.rb for the scheduled rake task. This gem assists with writing and deploying cron jobs.
 
-* Ruby version
+* Assumptions:
+ * admin user knows the correct data to add for each event
+ * admin users are already set up in the database, they cannot sign up for themselves
+ * additional API calls can be made from the worker file, syndication.rb, as needed
 
-* System dependencies
+ * Technologies used:
+	* Ruby on Rails (API only)
+	* PostgreSQL
+	* JWT Token (for admin authentication)
 
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,10 +2,9 @@ class Api::V1::ProductsController < ApplicationController
 
   def create
     if params["name"]["value"].length < 1 || params["description"]["value"].length < 1 || params["category"]["text"].length < 1 || params["priceDetails"]["value"].length < 1
-      render json: {msg: 'Please make sure you fill out all required fields in order to create a user!', result: 'Fail'}
+      render json: {msg: 'Please make sure you fill out all required fields in order to create a product!', result: 'Fail'}
     else
-      Product.create(name: params["name"]["value"], price: params["priceDetails"]["value"], start_date: params["startDate"]["value"], end_date: params["endDate"]["value"], category: params["category"]["value"], sub_category: params["subCategory"]["value"], start_time: params["startTime"]["value"], end_time: params["endTime"]["value"], location: params["location"]["value"], user_id: current_user.id)
-      byebug
+      Product.create(name: params["name"]["value"], description: params["description"]["value"], price: params["priceDetails"]["value"], start_date: params["startDate"]["value"], end_date: params["endDate"]["value"], category: params["category"]["text"], sub_category: params["subCategory"]["text"], start_time: params["startTime"]["value"], end_time: params["endTime"]["value"], location: params["location"]["value"], user_id: current_user.id)
       render json: {msg: 'Product Successfully Created', result: 'Success'}
     end
   end
